17b3671b5cef91de3f13b9f0b748069277a80568
BarshaSingh24 <singhbarsha879@gmail.com>
2024-10-02 11:00:31 +0000

    Add 02_datatypes.js with basic datatype examples

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -4,7 +4,17 @@
 let name="barsha"//string datatype
 let age=22//number datatype
 let isLoggedIn=false//boolean datatype
-
+//*Primitive datatypes
 //number=>2 to power 53
 //bigint=>used when the no is too large
 //string=>"" or '' but prefer double quotes.
+//boolean=>true/false
+//null=>standalone value->it is a representation of an empty value
+//undefined=>variable is declared but value is not assigned
+//symbol=>used for uniqueness
+
+console.log(typeof "barsha");//will print string
+console.log(typeof age);//number
+
+console.log(typeof null);//will print object
+console.log(typeof undefined);//undefined
