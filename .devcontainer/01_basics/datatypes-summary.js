1e56d0ce81574472189a7202ab3304c2b76983ba
BarshaSingh24 <singhbarsha879@gmail.com>
2024-10-17 15:04:19 +0000

    Add datatypes-summary.js

--- .devcontainer/01_basics/datatypes-summary.js
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -6,7 +6,7 @@ Primitive data types are passed by value, meaning when they are copied from one 
 
 const id=Symbol('123');
 const anotherId=Symbol('123');
-console.log(id===anotherId)//false as symbol will make both the id's unique.
+//console.log(id===anotherId)//false as symbol will make both the id's unique.
 
 /*Non-primitive data types are passed by reference, meaning they hold a reference to the memory location where the actual data is stored. As a result, changes made to the data through one reference will be reflected in all references to the same data." 
 
@@ -21,7 +21,25 @@ let myobj={
 
 //one way to declare func is treating function as a variable in js
 const myfunction=function(){
-    console.log("Hello world");
+    //console.log("Hello world");
 }
-console.log(typeof myfunction)//output-function which is basically called object function.
+//console.log(typeof myfunction)//output-function which is basically called object function.
 //datatype of non-primitive datatypes is object.
+//++++++++++++++++++++++++++++++++++++++++
+// Stack(primitive), Heap(Non-Primitive)
+let myName="Barsha"
+let myanotherName=myName;
+myanotherName="Shivani"
+console.log(myName);//Barsha
+console.log(myanotherName);//Shivani
+//so myName remains unaffected since it is of primitive data type so the changes are made in the copy of it.
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+let userTwo= userOne
+userTwo.email = "barsha@google.com"
+console.log(userOne.email);//barsha@google.com
+console.log(userTwo.email);//barsha@google.com
+//As in case of heap both userOne and userTwo will get the same reference of email & upi.
