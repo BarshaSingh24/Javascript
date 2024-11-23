a483d10d5eee0d2bd54bee9f9922dd8296378896
BarshaSingh24 <singhbarsha879@gmail.com>
2024-11-23 19:28:44 +0000

    Add 05_string.js for string operations

---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,14 @@
+const num=400;
+//console.log(num);//400
+const balance=new Number(100);
+//console.log(balance);//[Number: 100]
+//console.log(balance.toString().length);//3
+console.log(balance.toFixed(2));//100.00
+const num1=23.8966
+const num2=123.8966
+console.log(num1.toPrecision(3));//23.9
+console.log(num2.toPrecision(3));//124
+const num3=1123.8966
+console.log(num3.toPrecision(3));//1.12e+3->upto 3 digits precision and the next is converted to exponential
+
+
