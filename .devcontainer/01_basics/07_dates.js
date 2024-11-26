dd8826400c549de3508293b60a5c316247b552a2
BarshaSingh24 <singhbarsha879@gmail.com>
2024-11-26 16:14:37 +0000

    Dates in js

---
+++ .devcontainer/01_basics/07_dates.js
@@ -0,0 +1,6 @@
+//Dates
+let myDate=new Date()
+console.log(myDate);//we get a date object that is not readable or comparable or usable so our focus would be how to convert this date in the useable form->2024-11-26T10:35:48.973Z
+console.log(myDate.toString());//Tue Nov 26 2024 10:35:48 GMT+0000 (Coordinated Universal Time)better than the previous form 
+//Major DateString Functions
+//toDateString,toISOString,toJSON,toLocaleDateString,toLocaleString
