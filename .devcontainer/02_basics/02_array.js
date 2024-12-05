671110d3b5e60baab0ed54337aa19d3720975b8e
BarshaSingh24 <singhbarsha879@gmail.com>
2024-12-05 09:51:49 +0000

    basics of array covered

--- .devcontainer/02_basics/02_array.js
+++ .devcontainer/02_basics/02_array.js
@@ -16,7 +16,7 @@ const all_new_heros = [...marvel_heros, ...dc_heros]
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);/*[
+/*console.log(real_another_array);[
     1, 2, 3, 4, 5,
     6, 7, 6, 7, 4,
     5
@@ -24,12 +24,12 @@ console.log(real_another_array);/*[
 
 
 
-/*console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Hitesh"))//false
+console.log(Array.from("Hitesh"))//[ 'H', 'i', 't', 'e', 's', 'h' ]
+console.log(Array.from({name: "hitesh"})) //[] interesting-coz we have to mention whether to make array out of the keys,or the values
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));*/
+//console.log(Array.of(score1, score2, score3));//[100,200,300]
